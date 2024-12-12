658f2993335cd0dee7f367d1987d442c1b32c88e
Manar Alghamdi <manar.a.gh@hotmail.com>
2024-12-12 14:52:08 +0300

    Initial Commit

---
+++ SoundSand/SoundSandApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundSandApp.swift
+//  SoundSand
+//
+//  Created by Manar Alghmadi on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SoundSandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
