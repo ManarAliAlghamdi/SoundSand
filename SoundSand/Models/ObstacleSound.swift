9314812af4d5c5dc6577c2ac8d0a44c611b6bbe5
Manar Alghamdi <manar.a.gh@hotmail.com>
2024-12-16 02:30:49 +0300

    Create Events and ObstacleSound models.
    Implement swipe gestures to switch lanes.
    Play sounds to indicate the user's current lane.
    Organize sound files for better project structure.

---
+++ SoundSand/Models/ObstacleSound.swift
@@ -0,0 +1,15 @@
+//
+//  eventSound.swift
+//  SoundSand
+//
+//  Created by Manar Alghmadi on 15/12/2024.
+//
+
+import Foundation
+
+// Represents the sounds associated with an obstacle in the game.
+class ObstacleSound {
+    var soundName: String = ""
+    var laneNo: Int = 0
+    var soundStatus: Bool = false
+}
